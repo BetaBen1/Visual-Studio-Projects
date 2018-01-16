1a900eded5e23b83014f04f26cd2f657c16e41bc
BetaBen1 <bbhardin1@gmail.com>
2018-01-16 15:44:31 -0500

    Texture Work that Doesn't Work :(

--- Projects/OpenGL Tests/OpenGL Tests/Texture.cpp
+++ Projects/OpenGL Tests/OpenGL Tests/Texture.cpp
@@ -6,22 +6,21 @@
 Texture::Texture(const std::string& fileName)
 {
 	int width, height, numComponents;
-	unsigned char* imageData = stbi_load(fileName.c_str(), &width, &height, &numComponents, 4);
+	unsigned char* imageData = stbi_load((fileName).c_str(), &width, &height, &numComponents, 4);
 
 	if (imageData == NULL) 
 	{
-		
 		std::cerr << "Texture loading failed for texture: " << fileName << std::endl;
 	}
 
 	glGenTextures(1, &m_texture);
 	glBindTexture(GL_TEXTURE_2D, m_texture);
 
-	glTextureParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-	glTextureParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
 	
-	glTextureParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-	glTextureParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 
 	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, imageData);
 
